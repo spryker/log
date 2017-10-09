45c6cc525e37914eaf4cdd10bd3b589237c1af0e
René Klatt <rene.klatt@spryker.com>
2017-10-09 09:58:27 +0000

    core-2001 Added new Frontend Module, added missing console commands to Modules

---
+++ src/Spryker/Zed/Log/Business/LogFacadeInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Log\Business;
+
+interface LogFacadeInterface
+{
+
+    /**
+     * Specification:
+     * - Deletes all specified log directories.
+     *
+     * @api
+     *
+     * @return void
+     */
+    public function deleteLogFileDirectories();
+
+}
---
+++ src/Spryker/Zed/Log/Communication/Console/DeleteLogFilesConsole.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Log\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Log\Business\LogFacade getFacade()
+ */
+class DeleteLogFilesConsole extends Console
+{
+
+    const COMMAND_NAME = 'log:delete:log-file-directories';
+    const DESCRIPTION = 'This command will delete all log files and their directory.';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->info('Delete log files');
+        $this->getFacade()->deleteLogFileDirectories();
+    }
+
+}
